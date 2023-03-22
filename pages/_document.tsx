8f184c18e0c9d297fd0a35235b24414e86b5505c
AzriZzz <lenovorosak@gmail.com>
2023-03-22 17:28:39 +0800

    Add google analytics for tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,33 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+/* eslint-disable @next/next/next-script-for-ga */
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-Q7YKXGM3W4"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-Q7YKXGM3W4');
+              `,
+            }}
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
