7a68a4070aa307bece0b4cc4b2f73434df2f4d68
AzriZzz <lenovorosak@gmail.com>
2023-03-22 12:11:17 +0800

    Add image on header

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,13 +1,23 @@
 import { title, visitTitle, youtubeLink } from '@/constants/data'
 import React from 'react'
+import Image from 'next/image';
 
 const Header = () => {
   return (
-    <header>
-      <h1 className='flex items-center justify-center py-5 text-2xl font-bold md:text-3xl w-1/'>
+    <header className='flex justify-center'>
+      <h1 className='relative flex py-5 text-2xl font-bold md:text-3xl'>
         <a target="_blank" href={youtubeLink} rel="noopener noreferrer" title={visitTitle}>
           {title}
         </a>
+        <div className='ml-3'>
+          <Image
+            src="/images/mortar.png"
+            alt="Mortar icons created by Freepik - Flaticon"
+            width={30}
+            height={30}
+            priority
+          />
+        </div>
       </h1>
     </header>
   )
