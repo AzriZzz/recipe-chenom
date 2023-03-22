28fc5baa92076b384de8e8a9a302cd7ccf7d95c2
AzriZzz <lenovorosak@gmail.com>
2023-03-22 22:59:43 +0800

    Fix: Tweak title size on tab to large screen size

--- components/components/Video.tsx
+++ components/components/Video.tsx
@@ -16,7 +16,7 @@ const Video: React.FC<IVideo> = ({
 }) => {
 
   return (
-    <div className="w-full md:w-[250px] h-[270px] md:h-[190px] md:p-0">
+    <div className="w-full md:w-[250px] h-[270px] md:h-[200px] md:p-0">
       <div className="w-full">
         <a href={videoUrl} target="_blank" rel="noopener noreferrer">
           <div className="relative w-full h-[210px] md:h-[140px]  md:rounded overflow-hidden transition-transform duration-300 lg:hover:scale-105">
@@ -30,7 +30,7 @@ const Video: React.FC<IVideo> = ({
           </div>
         </a>
         <div className="relative flex flex-row justify-between mt-2 align-top items-top">
-          <div className="pl-2 text-sm font-bold md:pl-0 text-primary-dark-blue md:text-xs 2xl:text-sm">
+          <div className="pl-2 text-sm font-bold md:pl-0 text-primary-dark-blue md:text-sm 2xl:text-sm">
             <a href={videoUrl} target="_blank" rel="noopener noreferrer">
               <p className="line-clamp-2" dangerouslySetInnerHTML={{ __html: useSanitize(title) }} />
             </a>
