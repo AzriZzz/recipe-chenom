01f5ae59139204c5a03d5a327e291acf73cf9b30
AzriZzz <lenovorosak@gmail.com>
2023-03-23 23:49:05 +0800

    Fix: tweak ui for filer

--- components/components/Filter.tsx
+++ components/components/Filter.tsx
@@ -4,11 +4,11 @@ import React from 'react'
 const Filter: React.FC<FilterProps> = ({ data, currentFilter, setCurrentFilter }) => {
   return (
     <div className=''>
-      <ul className="flex flex-wrap items-center justify-center px-5 font-semibold cursor-pointer lg:flex-row gap-x-2 md:gap-x-5 md:text-lg lg:px-0 ">
+      <ul className="flex flex-wrap items-center justify-center px-5 text-sm font-semibold cursor-pointer lg:flex-row gap-x-2 md:gap-x-5 lg:px-0 md:text-sm">
         {data.map((item: string) => (
           <li
             key={item}
-            className={`px-2 transition-all duration-300 py-1 ease-in-out text-sm md:text-sm hover:font-semibold ${currentFilter === item ? 'font-semibold text-sm bg-primary-dark-blue text-neutral-white   rounded' : ''
+            className={`px-2 py-1 ease-in-out ${currentFilter === item ? 'bg-primary-dark-blue text-neutral-white rounded' : ''
               }`}
           >
             <button
